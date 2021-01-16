60756deb6cb941a370de87b23005a92095b88d32
ashill99 <admshng@gmail.com>
2021-01-16 13:48:53 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,51 @@ function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
 
+  function onAdoptPet(id) {
+    console.log(id)
+    const updatedPets = pets.map((pet) => {
+      return pet.id === id ? {...pet, isAdopted: true} : pet  
+      })
+      setPets(updatedPets)
+  }
+
+  function onChangeType(e) {
+    console.log('type changed')
+    setFilters(e.target.value)
+  }
+
+  function onFindPetsClick() {
+    console.log(filters)
+    if (filters === "all") {
+      fetch("http://localhost:3001/pets")
+      .then(r => r.json())
+      .then((petsArray) => {
+        setPets(petsArray)
+      })
+    } else if 
+      (filters === "cat") {
+        fetch("http://localhost:3001/pets?type=cat")
+        .then(r => r.json())
+        .then((petsArray) => {
+          setPets(petsArray)
+        })
+      } else if 
+      (filters ==="dog") {
+      fetch("http://localhost:3001/pets?type=dog")
+      .then(r => r.json())
+      .then((petsArray) => {
+        setPets(petsArray)
+      })
+    } else if 
+    (filters ==="micropig") {
+      fetch("http://localhost:3001/pets?type=micropig")
+    .then(r => r.json())
+    .then((petsArray) => {
+      setPets(petsArray)
+    })
+    }
+  }
+
   return (
     <div className="ui container">
       <header>
@@ -15,10 +60,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onChangeType={onChangeType} onFindPetsClick={onFindPetsClick}/>
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser onAdoptPet={onAdoptPet} pets={pets}/>
           </div>
         </div>
       </div>
